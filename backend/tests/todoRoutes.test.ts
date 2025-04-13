1255d6ad249d1a7bd6e4a73ed7ae94e4cde8c1ba
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-04-13 09:56:19 +0300

    test(todos): write initial test cases for planned features

--- backend/tests/todoRoutes.test.ts
+++ backend/tests/todoRoutes.test.ts
@@ -8,4 +8,50 @@ describe('GET /todos', () => {
         expect(response.status).toBe(200)
         expect(response.body).toHaveLength(3)
     })
+
+    // it('should return todos that are completed', async () => {
+        
+    // })
+
+    // it('should return todos that are not completed', async () => {
+        
+    // })
 })
+
+// describe('DELETE /todos', () => {
+//     it('should delete a single todo', async () => {
+        
+//     })
+    
+//     it('should delete a all todos', async () => {
+
+//     })
+// })
+
+// describe('POST /todos', () => {
+//     it('should not create an empty todo', async () => {
+
+//     })
+
+//     it('should create a todo that has name', async () => {
+
+//     })
+    
+//     it('should not create a todo that already exists', async () => {
+
+//     })
+
+//     it('should not create a todo that has too many characters', async () => {
+
+//     })
+// })
+
+// describe('PUT /todos', () => {
+//     it('should be able to be edited', async () => {
+
+//     })
+
+//     it('should change the state for a single todo when done', async () => {
+        
+//     })
+// })
