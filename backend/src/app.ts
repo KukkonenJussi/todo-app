abc1b0460f137a3cdc6ebd1801e8794ae18960ee
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-04-17 12:59:43 +0300

    remove unused ping route and test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-import pingRoute from './routes/ping'
 import todoRoute from './routes/todoRoutes'
 
 const app = express();
 app.use(express.json());
 
-app.use('/ping', pingRoute)
 app.use('/todos', todoRoute)
 
 export default app;
--- backend/src/routes/ping.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/', (_request, response) => {
-    response.status(200).send('pong')
-})
-
-export default router
