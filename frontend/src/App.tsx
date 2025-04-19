cfe95807dfb01a8e4103a46d9548dfe9ee12adcf
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-04-19 10:22:28 +0300

    chore(env): configure environment variables for API base URL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,15 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL
+
 const App = () => {
+
+  useEffect(() => {
+    axios.get(`${baseUrl}/todos`).then((response) => {
+      console.log(response.data)
+    })
+  })
 
   return (
     <div>
