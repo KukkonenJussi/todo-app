f569b426ce964a9e448926554de46374c0482e15
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-04-21 10:27:45 +0300

    feat: add temporary todo creation form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import todoService from "./services/todoService";
 import { TodoItem } from "./types";
 import TodoList from "./components/TodoList";
 
 const App = () => {
   const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [name, setName] = useState("");
 
   useEffect(() => {
     todoService.getAllTodos().then((data) => {
@@ -12,10 +13,23 @@ const App = () => {
     });
   }, []);
 
+  const todoCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    todoService.createTodo({ name }).then((data) => {
+      setTodos(todos.concat(data));
+      setName('')
+    })
+  };
+
   return (
     <div>
       <h1>Todo App</h1>
-      <TodoList todos={todos}/>
+      <form onSubmit={todoCreation}>
+        <input value={name} onChange={(event) => setName(event.target.value)} />
+        <button type="submit">Add</button>
+      </form>
+      <TodoList todos={todos} />
     </div>
   );
 };
