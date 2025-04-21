ba22a3d51e9750b7c497316704dcff58eca8f347
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-04-21 12:47:14 +0300

    feat: show error message when todo creation fails

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,11 @@ import todoService from "./services/todoService";
 import { TodoItem } from "./types";
 import TodoList from "./components/TodoList";
 import AddTodoForm from "./components/AddTodoForm";
+import axios from "axios";
 
 const App = () => {
   const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
     todoService.getAllTodos().then((data) => {
@@ -13,16 +15,28 @@ const App = () => {
     });
   }, []);
 
-  const todoCreation = (name: string) => {
-    todoService.createTodo({ name }).then((data) => {
-      setTodos(todos.concat(data));
-    });
+  const todoCreation = async (name: string) => {
+    try {
+      await todoService.createTodo({ name }).then((data) => {
+        setTodos(todos.concat(data));
+      });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setMessage(error.response?.data.error);
+        setTimeout(() => {
+          setMessage(null);
+        }, 3000);
+      } else {
+        console.log(error);
+      }
+    }
   };
 
   return (
     <div>
       <h1>Todo App</h1>
-      <AddTodoForm onSubmit={todoCreation}/>
+      {message}
+      <AddTodoForm onSubmit={todoCreation} />
       <TodoList todos={todos} />
     </div>
   );
