b1beea6fa49316be73fbe273cce6aadc72e4c5ae
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-04-22 12:45:11 +0300

    test(e2e): add basic visibility test for front page header

--- frontend/e2e/todo_app.spec.ts
+++ frontend/e2e/todo_app.spec.ts
@@ -1,9 +1,20 @@
 import { test, expect } from "@playwright/test";
 
-const apiUrl = "http://localhost:5173/"
+const apiUrl = "http://localhost:5173/";
 
-test("front page can be opened", async ({ page }) => {
-  await page.goto(apiUrl);
+test.describe("Todo App", () => {
+  test("front page can be opened", async ({ page }) => {
+    await page.goto(apiUrl);
 
-  await expect(page).toHaveTitle(/Todo App/);
+    await expect(page).toHaveTitle(/Todo App/);
+  });
+
+  test("header is visible", async ({ page }) => {
+    await page.goto(apiUrl);
+
+    const header = page.getByRole('heading', { name: 'Todo App' })
+
+    await expect(header).toBeVisible()
+    await expect(header).toHaveText('Todo App')
+  });
 });
