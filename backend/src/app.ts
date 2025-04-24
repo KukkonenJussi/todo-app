7a7bd58abc4eb46841be164cc46b8933b4b88131
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-04-24 19:42:07 +0300

    chore(app): format code to look prettier

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,18 @@
-import express from 'express';
-import todoRoute from './routes/todoRoutes'
-import cors from 'cors';
+import express from "express";
+import todoRoute from "./routes/todoRoutes";
+import cors from "cors";
 
-const helmet = require('helmet')
+const helmet = require("helmet");
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  };
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
 
-app.use(cors(corsOptions))
-app.use(helmet())
+app.use(cors(corsOptions));
+app.use(helmet());
 app.use(express.json());
 
-app.use('/todos', todoRoute)
+app.use("/todos", todoRoute);
 
 export default app;
