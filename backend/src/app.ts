d5591efbcf8569deeaef3396d5f3e68f08f65ebe
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-04-24 18:11:32 +0300

    feat(security): enable CORS and add Helmet for basic security headers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,15 @@ import express from 'express';
 import todoRoute from './routes/todoRoutes'
 import cors from 'cors';
 
+const helmet = require('helmet')
 const app = express();
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  };
 
-app.use(cors())
+app.use(cors(corsOptions))
+app.use(helmet())
 app.use(express.json());
 
 app.use('/todos', todoRoute)
