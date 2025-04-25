3afd5cf002248860f8fe0be85354e0003d449931
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-04-25 08:51:24 +0300

    test(EditDialog): add unit tests for rendering elements and input value

---
+++ frontend/src/components/__tests__/EditDialog.test.tsx
@@ -0,0 +1,46 @@
+import { describe, expect, it, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import EditDialog from "../EditDialog";
+
+describe("EditDialog", () => {
+  const mockOnClose = vi.fn();
+  const mockOnConfirm = vi.fn();
+  const mockOnNameChange = vi.fn();
+
+  it("renders title, input field and buttons", async () => {
+    render(
+      <EditDialog
+        open={true}
+        onClose={mockOnClose}
+        onConfirm={mockOnConfirm}
+        editedName="Go outside and catch some air"
+        onNameChange={mockOnNameChange}
+      />
+    );
+    
+    const editTitle = screen.getByRole('heading', { name: 'Edit Todo name' })
+    const inputField= screen.getByRole('textbox', { name: 'Todo name' })
+    const cancelButton = screen.getByRole('button', { name: 'Cancel' })
+    const saveButton = screen.getByRole('button', { name: 'Save' })
+
+    expect(editTitle).toBeInTheDocument()
+    expect(inputField).toBeInTheDocument()
+    expect(cancelButton).toBeInTheDocument()
+    expect(saveButton).toBeInTheDocument()
+  });
+
+  it("renders Todo with given name", async () => {
+    render(
+      <EditDialog
+        open={true}
+        onClose={mockOnClose}
+        onConfirm={mockOnConfirm}
+        editedName="Go outside and catch some air"
+        onNameChange={mockOnNameChange}
+      />
+    );
+
+    const inputField = screen.getByRole('textbox', { name: 'Todo name' })
+    expect(inputField).toHaveValue("Go outside and catch some air");
+  });
+});
