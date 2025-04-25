36ac1284145943e8740509e3a6e1db6f6f76e3f6
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-04-25 12:34:27 +0300

    feat: add slowLimiter middlewar to prevent request flooding

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import todoRoute from "./routes/todoRoutes";
 import cors from "cors";
+import slowDown from "express-slow-down";
 
 const helmet = require("helmet");
 const app = express();
@@ -9,8 +10,16 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
+const slowLimiter = slowDown({
+  windowMs: 15 * 60 * 1000,
+  delayAfter: 50,
+  maxDelayMs: 5 * 1000,
+  message: "Too many requests, please slow down!",
+});
+
 app.use(cors(corsOptions));
 app.use(helmet());
+app.use(slowLimiter);
 app.use(express.json());
 
 app.use("/todos", todoRoute);
