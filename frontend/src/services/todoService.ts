d8eb8d4e225f38e23d026f741f9da12beb84262e
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-04-30 12:47:17 +0300

    chore: refactor todoService methods to use await/async

--- frontend/src/services/todoService.ts
+++ frontend/src/services/todoService.ts
@@ -3,34 +3,34 @@ import { TodoItem, NewTodoItem } from "../types";
 
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
-export const getAllTodos = () => {
-  return axios
-    .get<TodoItem[]>(`${apiBaseUrl}/todos`)
-    .then((response) => response.data);
+export const getAllTodos = async () => {
+  const response = await axios
+    .get<TodoItem[]>(`${apiBaseUrl}/todos`);
+  return response.data;
 };
 
-export const createTodo = (object: NewTodoItem) => {
-  return axios
-    .post<TodoItem>(`${apiBaseUrl}/todos`, object)
-    .then((response) => response.data);
+export const createTodo = async (object: NewTodoItem) => {
+  const response = await axios
+    .post<TodoItem>(`${apiBaseUrl}/todos`, object);
+  return response.data;
 };
 
-export const deleteTodo = (id: string) => {
-  return axios
-    .delete<TodoItem>(`${apiBaseUrl}/todos/${id}`)
-    .then((response) => response.data);
+export const deleteTodo = async (id: string) => {
+  const response = await axios
+    .delete<TodoItem>(`${apiBaseUrl}/todos/${id}`);
+  return response.data;
 };
 
-export const deleteAllTodos = () => {
-  return axios
-    .delete<TodoItem[]>(`${apiBaseUrl}/todos`)
-    .then((response) => response.data);
+export const deleteAllTodos = async () => {
+  const response = await axios
+    .delete<TodoItem[]>(`${apiBaseUrl}/todos`);
+  return response.data;
 };
 
-export const updateTodo = (id: string, object: unknown) => {
-  return axios
-    .put<TodoItem>(`${apiBaseUrl}/todos/${id}`, object)
-    .then((response) => response.data);
+export const updateTodo = async (id: string, object: unknown) => {
+  const response = await axios
+    .put<TodoItem>(`${apiBaseUrl}/todos/${id}`, object);
+  return response.data;
 };
 
 export default {
