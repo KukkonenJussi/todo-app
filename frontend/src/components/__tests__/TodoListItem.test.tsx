548e02709f53bea06a3ded6704159d344757417c
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-05-04 12:38:29 +0300

    test(todoListItem): add unit tests for todo name and action buttons

--- frontend/src/components/__tests__/TodoListItem.test.tsx
+++ frontend/src/components/__tests__/TodoListItem.test.tsx
@@ -2,14 +2,14 @@ import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import TodoListItem from "../todo/TodoListItem";
 
+const todo = {
+  id: "1",
+  name: "Build a Todo App",
+  completed: false,
+};
+
 describe("TodoListItem", () => {
   it("renders the todo name passed as prop", () => {
-    const todo = {
-      id: "1",
-      name: "Build a Todo App",
-      completed: false,
-    };
-
     render(
       <TodoListItem
         todoItem={todo}
@@ -19,7 +19,55 @@ describe("TodoListItem", () => {
       />
     );
 
-    const element = screen.getByText("Build a Todo App");
-    expect(element).toBeDefined();
+    const todoNameElement = screen.getByText(/build a todo app/i);
+    expect(todoNameElement).toBeDefined();
+  });
+
+  it("renders the delete icon", () => {
+    render(
+      <TodoListItem
+        todoItem={todo}
+        onDelete={() => {}}
+        onUpdate={() => {}}
+        onCompletedUpdate={() => {}}
+      />
+    );
+
+    const deleteButton = screen.getByRole("button", {
+      name: /delete todo/i,
+    });
+    expect(deleteButton).toBeInTheDocument();
+  });
+
+  it("renders the edit icon", () => {
+    render(
+      <TodoListItem
+        todoItem={todo}
+        onDelete={() => {}}
+        onUpdate={() => {}}
+        onCompletedUpdate={() => {}}
+      />
+    );
+
+    const editButton = screen.getByRole("button", {
+      name: /edit todo/i,
+    });
+    expect(editButton).toBeInTheDocument();
+  });
+
+  it("renders the check icon", () => {
+    render(
+      <TodoListItem
+        todoItem={todo}
+        onDelete={() => {}}
+        onUpdate={() => {}}
+        onCompletedUpdate={() => {}}
+      />
+    );
+
+    const toggleStatusButton = screen.getByRole("button", {
+      name: /change completed status/i,
+    });
+    expect(toggleStatusButton).toBeInTheDocument();
   });
 });
--- frontend/src/components/todo/TodoListItem.tsx
+++ frontend/src/components/todo/TodoListItem.tsx
@@ -1,5 +1,5 @@
 import { TableCell, TableRow } from "@mui/material";
-import { TodoItem } from "../types";
+import { TodoItem } from "../../types";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
