86c6aa7f4e6b3f52327d22a22a73c96217df066c
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-05-07 18:33:13 +0300

    test(EditDialog): add input change handler test for EditDialog

--- frontend/src/components/__tests__/EditDialog.test.tsx
+++ frontend/src/components/__tests__/EditDialog.test.tsx
@@ -1,6 +1,7 @@
 import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import EditDialog from "../dialogs/EditDialog";
+import userEvent from "@testing-library/user-event";
 
 describe("EditDialog", () => {
   const mockOnClose = vi.fn();
@@ -17,16 +18,16 @@ describe("EditDialog", () => {
         onNameChange={mockOnNameChange}
       />
     );
-    
-    const editTitle = screen.getByRole('heading', { name: 'Edit Todo name' })
-    const inputField= screen.getByRole('textbox', { name: 'Todo name' })
-    const cancelButton = screen.getByRole('button', { name: 'Cancel' })
-    const saveButton = screen.getByRole('button', { name: 'Save' })
 
-    expect(editTitle).toBeInTheDocument()
-    expect(inputField).toBeInTheDocument()
-    expect(cancelButton).toBeInTheDocument()
-    expect(saveButton).toBeInTheDocument()
+    const editTitle = screen.getByRole("heading", { name: "Edit Todo name" });
+    const inputField = screen.getByRole("textbox", { name: "Todo name" });
+    const cancelButton = screen.getByRole("button", { name: "Cancel" });
+    const saveButton = screen.getByRole("button", { name: "Save" });
+
+    expect(editTitle).toBeInTheDocument();
+    expect(inputField).toBeInTheDocument();
+    expect(cancelButton).toBeInTheDocument();
+    expect(saveButton).toBeInTheDocument();
   });
 
   it("renders Todo with given name", async () => {
@@ -40,7 +41,25 @@ describe("EditDialog", () => {
       />
     );
 
-    const inputField = screen.getByRole('textbox', { name: 'Todo name' })
+    const inputField = screen.getByRole("textbox", { name: "Todo name" });
     expect(inputField).toHaveValue("Go outside and catch some air");
+  });
+
+  it("calls onNameChange when input value changes", async () => {
+    const user = userEvent.setup();
+    render(
+      <EditDialog
+        open={true}
+        onClose={mockOnClose}
+        onConfirm={mockOnConfirm}
+        editedName=""
+        onNameChange={mockOnNameChange}
+      />
+    );
+
+    const inputField = screen.getByRole("textbox", { name: "Todo name" });
+    await user.type(inputField, "Hello World");
+
+    expect(mockOnNameChange).toHaveBeenCalled();
   });
 });
