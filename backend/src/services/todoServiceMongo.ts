8263c5497499184183709851ceaf5aa32f8259ca
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-05-15 12:05:46 +0300

    feat(service): support filtering todos by userId

--- backend/src/services/todoServiceMongo.ts
+++ backend/src/services/todoServiceMongo.ts
@@ -1,7 +1,11 @@
 import Todo from "../models/Todo";
 
-const getAllTodos = async () => {
-  return await Todo.find({});
+const getAllTodos = async (userId?: string) => {
+  if (userId) {
+    return await Todo.find({ userId });
+  } else {
+    return await Todo.find({});
+  }
 };
 
 export default { getAllTodos };
--- backend/tests/todoServiceMongo.test.ts
+++ backend/tests/todoServiceMongo.test.ts
@@ -16,14 +16,17 @@ beforeEach(async () => {
     {
       name: "Build a Todo App",
       completed: false,
+      userId: "user1",
     },
     {
       name: "Learn TDD while doing this project",
       completed: false,
+      userId: "user1",
     },
     {
       name: "Remember to have a break",
       completed: true,
+      userId: "user2",
     },
   ]);
 });
@@ -32,8 +35,29 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-test("getAllTodos returns all todos", async () => {
-  const todos = await todoServiceMongo.getAllTodos();
-  expect(todos).toHaveLength(3);
-  expect(todos[0].name).toBe("Build a Todo App");
+describe("getAllTodos", () => {
+  it("returns all todos", async () => {
+    const todos = await todoServiceMongo.getAllTodos();
+
+    expect(todos).toHaveLength(3);
+    expect(todos[0].name).toBe("Build a Todo App");
+  });
+
+  it("returns only the todos that belong to the specified user", async () => {
+    const todos = await todoServiceMongo.getAllTodos("user1");
+
+    expect(todos).toHaveLength(2);
+  });
+
+  it("does not return todos belonging to other users", async () => {
+    const correctTodos = await todoServiceMongo.getAllTodos("user2");
+
+    expect(correctTodos.every((todo) => todo.userId === "user2")).toBe(true);
+  });
+
+  it("return an empty array if the user has no todos", async () => {
+    const todos = await todoServiceMongo.getAllTodos("unknown-user");
+
+    expect(todos).toEqual([]);
+  });
 });
