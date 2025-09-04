f4283c273b5491bfb6174ffe78db082006332352
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-09-04 08:31:45 +0300

    refactor(todoServiceMongo): add response type for getAllTodos axios request

--- frontend/src/services/todoServiceMongo.ts
+++ frontend/src/services/todoServiceMongo.ts
@@ -3,12 +3,14 @@ import { TodoItemMongoose } from "../types";
 
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
-export const getAllTodos = async (userId: string): Promise<TodoItemMongoose[]> => {
-  const response = await axios.get(`${apiBaseUrl}/todos`, {
+export const getAllTodos = async (
+  userId: string
+): Promise<TodoItemMongoose[]> => {
+  const response = await axios.get<TodoItemMongoose[]>(`${apiBaseUrl}/todos`, {
     params: { userId },
   });
 
-  return response.data
+  return response.data;
 };
 
 export default {
