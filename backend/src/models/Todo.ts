e2066be64f7bec7f4744afc01b1e190200596a0a
Jussi Kukkonen <jussi.kukko@gmail.com>
2025-09-06 10:26:57 +0300

    feat: link Todo model with User model

--- backend/src/models/Todo.ts
+++ backend/src/models/Todo.ts
@@ -4,6 +4,7 @@ const todoSchema = new mongoose.Schema({
   name: { type: String, required: true },
   completed: { type: Boolean, default: false },
   userId: { type: String },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
 const Todo = mongoose.model("Todo", todoSchema);
